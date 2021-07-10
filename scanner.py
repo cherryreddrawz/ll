17efc2f7fec247e3de3e16f2f47519460ee885d4
h0nda <41300123+h0nde@users.noreply.github.com>
2021-07-10 13:37:31 +0200

    add support for py2exe

--- scanner.py
+++ scanner.py
@@ -1,7 +1,9 @@
 from scanner.controllers import Controller
 from scanner.arguments import get_arguments
+import multiprocessing
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     arguments = get_arguments()
     controller = Controller(
         arguments=arguments
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+import py2exe
+
+with open("requirements.txt") as fp:
+    requirements = fp.read().splitlines()
+
+setuptools.setup(
+    name="gscanner",
+    author="h0nda",
+    url="https://github.com/h0nde/roblox-group-scanner-v2",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    install_requires=requirements,
+    console=["scanner.py"]
+)
