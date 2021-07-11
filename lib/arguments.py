c65e3e6834e5b9f586b736911a25459648335c60
h0nda <41300123+h0nde@users.noreply.github.com>
2021-07-11 09:42:44 +0200

    Update arguments.py

--- lib/arguments.py
+++ lib/arguments.py
@@ -16,13 +16,13 @@ def parse_range(range_string):
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--threads", type=int, default=50, help="Amount of threads to be created per worker")
-    parser.add_argument("-w", "--workers", type=int, default=cpu_count(), help="Amount of processes to be created")
-    parser.add_argument("-r", "--range", type=parse_range, required=True, help="Range of group ids to be scanned")
-    parser.add_argument("-c", "--cut-off", type=int, help="Group ids past this point won't be blacklisted based on their current validity status")
-    parser.add_argument("-p", "--proxy-file", type=argparse.FileType("r", encoding="UTF-8", errors="ignore"), help="List of HTTP proxies, separated by newline")
-    parser.add_argument("-u", "--webhook-url", type=str, help="Found groups will be posted to the specified webhook URL")
-    parser.add_argument("--chunk-size", type=int, default=100, help="Amount of groups to be sent per API request")
-    parser.add_argument("--timeout", type=float, default=5.0, help="Timeout for server connections and responses")
+    parser.add_argument("-t", "--threads", type=int, default=50, help="amount of threads per worker")
+    parser.add_argument("-w", "--workers", type=int, default=cpu_count(), help="amount of workers (processes)")
+    parser.add_argument("-r", "--range", type=parse_range, required=True, help="range of group ids to be scanned")
+    parser.add_argument("-c", "--cut-off", type=int, help="group ids past this point won't be blacklisted based on their current validity status")
+    parser.add_argument("-p", "--proxy-file", type=argparse.FileType("r", encoding="UTF-8", errors="ignore"), help="list of HTTP proxies, separated by newline")
+    parser.add_argument("-u", "--webhook-url", type=str, help="found groups will be posted to this url")
+    parser.add_argument("--chunk-size", type=int, default=100, help="amount of groups to be sent per API request")
+    parser.add_argument("--timeout", type=float, default=5.0, help="timeout for server connections and responses")
     arguments = parser.parse_args()
     return arguments
